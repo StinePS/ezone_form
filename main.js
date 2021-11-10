c5731f779cb6ee23cacc5df1f2fd4361c28f2387
Luca275b <luca275b@stud.kea.dk>
2021-11-10 15:42:37 +0100

    DB and post
    + value and classes for checkboxes

--- main.js
+++ main.js
@@ -3,6 +3,27 @@ import "./data.json";
 // import { greetings } from "./modules/greeting";
 // console.log(greetings);
 
+const endpoint = "https://mainbase-7e6d.restdb.io/rest/ezone";
+const apikey = "617696f28597142da1745a37";
+
+window.addEventListener("load", (e) => {
+  document.querySelector("button.submitBtn").addEventListener("click", () => {
+    const data = {
+      gamertag: document.querySelector("#gamertag").value,
+      email: document.querySelector("#email").value,
+      password: document.querySelector("#password").value,
+      age: document.querySelector("#age").value,
+      favouriteGame: document.querySelector("#favourite").value,
+      gameGenres: document.querySelector("#gametypes").value,
+      improvement: document.querySelector('.improveCheckbox:checked').value,
+      hoursGaming: document.querySelector("#hoursGaming").value,
+      sleep: document.querySelector("#sleep").value,
+      activity: document.querySelector("#physical").value,
+    };
+    postData(data);
+  });
+});
+
 window.addEventListener("DOMContentLoaded", siteLoaded);
 
 function siteLoaded() {}
@@ -44,4 +65,19 @@ function initCarousel(element) {
     pageIndex--;
     updateFormPages();
   });
+}
+
+function postData(data){
+  const postData = JSON.stringify(data);
+  fetch(endpoint, {
+  method: "post",
+  headers: {
+    "Content-Type": "application/json; charset=utf-8",
+    "x-apikey": apikey,
+    "cache-control": "no-cache"
+  },
+      body: postData
+    })
+      .then(res => res.json())
+      .then(data => console.log(data));
 }
